49b9bfda2548c40ea426fc9c134f6060de007bb7
Amaury Delille <amaurydelille92500@gmail.com>
2024-06-13 15:50:44 +0200

    changed .env

--- website/backend/services/db.services.js
+++ website/backend/services/db.services.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://amaurysavemytime:5v1KgI6YfrvQkT5u@cluster0.pwqhc89.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = process.env.MONGODB_URI;
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -20,5 +20,3 @@ async function connectToDatabase() {
 }
 
 module.exports = connectToDatabase;
-
-
