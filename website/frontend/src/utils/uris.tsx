6b89920f4a75fb28967d2f2cddf6a5bfbe9702d6
Amaury Delille <amaurydelille92500@gmail.com>
2024-06-13 10:53:23 +0200

    fixed baseUrl

--- website/frontend/src/utils/uris.tsx
+++ website/frontend/src/utils/uris.tsx
@@ -1,5 +1,5 @@
-export const signUpEndpoint = 'https://savemytime-production-client.vercel.app/auth/signup';
-export const authEndpoint = 'https://savemytime-production-client.vercel.app/auth';
-export const getUserEndpoint = (id) => `https://savemytime-production-client.vercel.app/profile/${id}`;
-export const baseUrl = 'https://savemytime-production-client.vercel.app/';
+export const signUpEndpoint = 'https://savemytime-production-server.vercel.app/auth/signup';
+export const authEndpoint = 'https://savemytime-production-server.vercel.app/auth';
+export const getUserEndpoint = (id) => `https://savemytime-production-server.vercel.app/profile/${id}`;
+export const baseUrl = 'https://savemytime-production-server.vercel.app';
 export const stripeCheckoutSessionEndpoint = `${baseUrl}/create-checkout-session`;
