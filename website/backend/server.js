6a55076cf770ca5b19f92204b353a3d674939a0e
Amaury Delille <amaurydelille92500@gmail.com>
2024-06-13 11:05:36 +0200

    modified cors

--- website/backend/server.js
+++ website/backend/server.js
@@ -8,12 +8,7 @@ const { port } = require("./utils/config");
 
 const app = express();
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-      next();
-    });
+app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/', userRouter);
