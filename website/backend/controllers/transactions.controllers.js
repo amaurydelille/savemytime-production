2c0f8ca2891ab97c03946458198c60de1abc0e70
Amaury Delille <amaurydelille92500@gmail.com>
2024-06-15 23:37:40 +0200

    added stripe webhook

--- website/backend/controllers/transactions.controllers.js
+++ website/backend/controllers/transactions.controllers.js
@@ -1,16 +1,25 @@
+const { ObjectId } = require('mongodb');
 const Transaction = require('../services/transaction.services');
+const Tokens = require('../services/token.services');
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+
+const tokensDict = {
+    999: 50,
+    2499: 150,
+    7999: 300
+};
+
+const planDict = {
+    999: 'Classic',
+    2499: 'Expert',
+    7999: 'Premium'
+};
 
 const CreateTransaction = async (req, res) => {
     try {
         const { user_id, amount } = req.body;
-        const planDict  = {
-            9.99: 'Classic',
-            24.99: 'Expert',
-            79.99: 'Premium'
-        }
-        const plan = planDict[amount];
-        await Transaction.Create({ userId: user_id, amount: amount });
+        const plan = planDict[amount * 100]; 
+        await Transaction.Create({ userId: new ObjectId(user_id), amount: amount });
         const session = await stripe.checkout.sessions.create({
             payment_method_types: ['card'],
             line_items: [
@@ -20,7 +29,7 @@ const CreateTransaction = async (req, res) => {
                         product_data: {
                             name: plan,
                         },
-                        unit_amount: Math.round(amount * 100), 
+                        unit_amount: Math.round(amount * 100),
                     },
                     quantity: 1,
                 },
@@ -37,6 +46,44 @@ const CreateTransaction = async (req, res) => {
     }
 };
 
+const HandleTransactionEvent = (req, res) => {
+    const sig = req.headers['stripe-signature'];
+    let event;
+
+    try {
+        event = stripe.webhooks.constructEvent(req.body, sig, process.env.STRIPE_ENDPOINT_SECRET);
+    } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`, err.message);
+        return res.sendStatus(400);
+    }
+
+    switch (event.type) {
+        case 'payment_intent.succeeded':
+            const paymentIntentSucceeded = event.data.object;
+            handlePaymentIntentSucceeded(paymentIntentSucceeded);
+            break;
+        case 'payment_intent.payment_failed':
+            const paymentIntentFailed = event.data.object;
+            handlePaymentIntentFailed(paymentIntentFailed);
+            break;
+        default:
+            console.log(`Unhandled event type ${event.type}`);
+    }
+
+    res.json({ received: true });
+};
+
+const handlePaymentIntentSucceeded = async (paymentIntent) => {
+    const tokensNumber = tokensDict[paymentIntent.amount_received];
+    await Transaction.Create({ userId: new ObjectId(paymentIntent.metadata.user_id), amount: paymentIntent.amount_received });
+    await Tokens.Create({ userId: new ObjectId(paymentIntent.metadata.user_id), amount: tokensNumber });
+};
+
+const handlePaymentIntentFailed = (paymentIntent) => {
+    // Handle payment failure
+};
+
 module.exports = {
-    CreateTransaction
+    CreateTransaction,
+    HandleTransactionEvent
 };
--- website/backend/routes/transactions.routes.js
+++ website/backend/routes/transactions.routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const Transactions = require('../controllers/transactions.controllers');
 
 router.post('/create-checkout-session', Transactions.CreateTransaction);
+router.post('/stripe-event-listening', bodyParser.raw({ type: 'application/json' }), Transactions.HandleTransactionEvent);
 
 module.exports = router;
