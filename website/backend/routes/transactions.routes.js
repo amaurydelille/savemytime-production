293e3189a6811602d4190f51f9e218b2bf4aa87d
Amaury Delille <amaurydelille92500@gmail.com>
2024-06-17 14:19:56 +0200

    added routes

--- website/backend/routes/transactions.routes.js
+++ website/backend/routes/transactions.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Transactions = require('../controllers/transactions.controllers');
+const bodyParser = require('body-parser');
 
 router.post('/create-checkout-session', Transactions.CreateTransaction);
 router.post('/stripe-event-listening', bodyParser.raw({ type: 'application/json' }), Transactions.HandleTransactionEvent);
