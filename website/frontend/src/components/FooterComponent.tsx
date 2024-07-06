eeedfac9a1ba834cbaf2d2b825eb0b2a7adc8229
Amaury Delille <amaurydelille92500@gmail.com>
2024-07-06 15:53:43 +0200

    footer fix 3

--- website/frontend/src/components/FooterComponent.tsx
+++ website/frontend/src/components/FooterComponent.tsx
@@ -7,7 +7,7 @@ const FooterComponent = () => {
     return(
         <div className="footer-container">
             <p className='footer-title'>SaveMyTime 2024</p>
-            <a href='https://x.com/savemytime_app'><XOutlined />Join us on X</a>
+            <a href='https://x.com/savemytime_app'><XOutlined style={{ marginRight: 3 }}/> Join us on X</a>
             <a href='#'>Support us on ProductHunt</a>
 
         </div>
