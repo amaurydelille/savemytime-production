aed51a1d3be1b1da39baf4e1b2fd581d4e1f86a1
Amaury Delille <amaurydelille92500@gmail.com>
2024-07-06 16:07:16 +0200

    notification 2

--- website/frontend/src/pages/HomePage/HomePage.tsx
+++ website/frontend/src/pages/HomePage/HomePage.tsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import './HomePage.css';
 import { DownCircleOutlined, RocketTwoTone } from '@ant-design/icons';
 import { Badge, notification } from 'antd';
 import PricingCard from '../../components/PricingCardComponent.tsx';
 import type { NotificationArgsProps } from 'antd';
 
-const HomePage = () => {
+const NotificationContext = React.createContext({ name: 'Default' });
 
+const HomePage = () => {
     const scrollToGifContainer = () => {
         const gifContainer = document.getElementById('gif-container');
         if (gifContainer) {
@@ -16,63 +17,65 @@ const HomePage = () => {
 
     type NotificationPlacement = NotificationArgsProps['placement'];
     const [api, contextHolder] = notification.useNotification();
-    const Context = React.createContext({ name: 'Default' });
 
     const openNotification = (placement: NotificationPlacement) => {
         api.info({
-          message: `Notification ${placement}`,
-          description: <Context.Consumer>{({ name }) => `Hello, ${name}!`}</Context.Consumer>,
-          placement,
+            message: `Notification ${placement}`,
+            description: <NotificationContext.Consumer>{({ name }) => `Hello, ${name}!`}</NotificationContext.Consumer>,
+            placement,
         });
     };
 
     useEffect(() => {
         openNotification('bottomRight');
-    })
+    }, []);
 
     return (
-        <div className='main-container'>
-            <div className="presentation-container">
-                <div className="text-container">
-                    <p className='title'>SaveMyTime</p>
-                    <p className='text'>The most efficient, accurate, and reliable text summarizer<br />in a Google Chrome extension.</p>
-                    <a 
-                        onClick={scrollToGifContainer}
-                        className='button'
-                    >
-                        See Launch Promotion <RocketTwoTone style={{ fontSize: 20, marginLeft: 5 }} twoToneColor={'violet'} />
-                    </a>
+        <NotificationContext.Provider value={{ name: 'User' }}>
+            {contextHolder}
+            <div className='main-container'>
+                <div className="presentation-container">
+                    <div className="text-container">
+                        <p className='title'>SaveMyTime</p>
+                        <p className='text'>The most efficient, accurate, and reliable text summarizer<br />in a Google Chrome extension.</p>
+                        <a 
+                            onClick={scrollToGifContainer}
+                            className='button'
+                        >
+                            See Launch Promotion <RocketTwoTone style={{ fontSize: 20, marginLeft: 5 }} twoToneColor={'violet'} />
+                        </a>
+                    </div>
+                    <DownCircleOutlined className='down-button' onClick={scrollToGifContainer}/>
                 </div>
-                <DownCircleOutlined className='down-button' onClick={scrollToGifContainer}/>
-            </div>
 
-            <div id="gif-container" className="gif-container">
-                <p className='title'>Start saving your time, work efficiently</p>
-                <p className='text'>
-                    Tired of long reading webpages and inaccurate text summarizer ?
-                    With SaveMyTime, type keywords and get the summarized text.
-                </p>
-                <img
-                    src={require('../../images/presentation-video.gif')}  alt=''  
-                />
-            </div>
+                <div id="gif-container" className="gif-container">
+                    <p className='title'>Start saving your time, work efficiently</p>
+                    <p className='text'>
+                        Tired of long reading webpages and inaccurate text summarizer ?
+                        With SaveMyTime, type keywords and get the summarized text.
+                    </p>
+                    <img
+                        src={require('../../images/presentation-video.gif')} alt=''  
+                    />
+                </div>
 
-            <div className="pricing-container">
-                <p className='title' style={{ marginBottom: 20 }}>Plans and Pricing</p>
-                <div className="offers-container">
-                    <Badge.Ribbon text='-5$' color='blue'>
-                        <PricingCard offerIndex={1} />
-                    </Badge.Ribbon>
-                    <Badge.Ribbon text='-5$' color='blue'>
-                        <PricingCard offerIndex={2} />
-                    </Badge.Ribbon>
-                    <Badge.Ribbon text='-5$' color='blue'>
-                        <PricingCard offerIndex={3} />
-                    </Badge.Ribbon>
+                <div className="pricing-container">
+                    <p className='title' style={{ marginBottom: 20 }}>Plans and Pricing</p>
+                    <div className="offers-container">
+                        <Badge.Ribbon text='-5$' color='blue'>
+                            <PricingCard offerIndex={1} />
+                        </Badge.Ribbon>
+                        <Badge.Ribbon text='-5$' color='blue'>
+                            <PricingCard offerIndex={2} />
+                        </Badge.Ribbon>
+                        <Badge.Ribbon text='-5$' color='blue'>
+                            <PricingCard offerIndex={3} />
+                        </Badge.Ribbon>
+                    </div>
                 </div>
             </div>
-        </div>
+        </NotificationContext.Provider>
     );
-}
+};
 
 export default HomePage;
